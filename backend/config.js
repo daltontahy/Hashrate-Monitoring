f559900aa4070e68f657bcb914050dabdce007f8
daltontahy <132478425+daltontahy@users.noreply.github.com>
2024-04-06 17:27:01 -0400

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -2,8 +2,8 @@
 require('dotenv').config();
 
 module.exports = {
-    apiHost: process.env.API_HOST, // use https://api2.nicehash.com for production
-    apiKey: process.env.API_HOST, // get it here: https://test.nicehash.com/my/settings/keys or https://www.nicehash.com/my/settings/keys
+    apiHost: 'https://api2.nicehash.com', // use https://api2.nicehash.com for production
+    apiKey: process.env.API_KEY, // get it here: https://test.nicehash.com/my/settings/keys or https://www.nicehash.com/my/settings/keys
     apiSecret: process.env.API_SECRET, // get it here: https://test.nicehash.com/my/settings/keys or https://www.nicehash.com/my/settings/keys
     orgId: process.env.ORG_ID, // get it here: https://test.nicehash.com/my/organization or https://www.nicehash.com/my/organization
 };
