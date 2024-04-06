7936d06876036b9ff5df333dfc560d32549f9a7c
daltontahy <132478425+daltontahy@users.noreply.github.com>
2024-04-06 16:18:27 -0400

    create .env / sensitive info in gitignore

--- backend/config.js
+++ backend/config.js
@@ -1,8 +1,9 @@
 // config.js
+require('dotenv').config();
 
 module.exports = {
-    apiHost: 'https://api2.nicehash.com', // use https://api2.nicehash.com for production
-    apiKey: '56276274-3284-4ec4-8493-1aed26486444', // get it here: https://test.nicehash.com/my/settings/keys or https://www.nicehash.com/my/settings/keys
-    apiSecret: '68a9f4a6-5d65-40e3-9072-e35784399cbaa6fe144d-8783-468f-aac5-c021740391e3',
-    orgId: '2cf86342-22cf-4b06-86b7-38853cd7541e'
+    apiHost: process.env.API_HOST, // use https://api2.nicehash.com for production
+    apiKey: process.env.API_HOST, // get it here: https://test.nicehash.com/my/settings/keys or https://www.nicehash.com/my/settings/keys
+    apiSecret: process.env.API_SECRET, // get it here: https://test.nicehash.com/my/settings/keys or https://www.nicehash.com/my/settings/keys
+    orgId: process.env.ORG_ID, // get it here: https://test.nicehash.com/my/organization or https://www.nicehash.com/my/organization
 };
